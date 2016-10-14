6edb9524acb9e6e7b3b4160df57b09c8e2dbd2c2
Ally Hume <a.hume@ed.ac.uk>
2016-10-14 14:57:44 +0100

    Added a few new gal1 plots.

--- Examples/In_Silico_Loop/gal1_plot.m
+++ Examples/In_Silico_Loop/gal1_plot.m
@@ -15,6 +15,45 @@ for i=1:length(best_global_theta)
 
 end
 
+% Plot the relative confidence intervals for the parameters using only the
+% real part for complex numbers.
+figure;
+for i=1:length(best_global_theta)
+    model.par_names(i,:)
+    subplot(3,3,i);
+    clear y;
+    for j=2:exps.n_exp
+        % Some of these numbers are complex which is a little strange
+        y(j-1) = real(pe_results{j}.fit.rel_conf_interval(i));
+    end
+    x=[2:exps.n_exp];
+     x
+     y
+    plot(x,y);
+    title(model.par_names(i,:));
+
+end
+
+% Plot the relative confidence intervals for the parameters using the
+% complex magnitide for complex numbers.
+figure;
+for i=1:length(best_global_theta)
+    model.par_names(i,:)
+    subplot(3,3,i);
+    clear y;
+    for j=2:exps.n_exp
+        % Some of these numbers are complex which is a little strange
+        y(j-1) = abs(pe_results{j}.fit.rel_conf_interval(i));
+    end
+    x=[2:exps.n_exp];
+     x
+     y
+    plot(x,y);
+    title(model.par_names(i,:));
+
+end
+
+
 % Plot the initial conditions convergence to truth
 figure;
 for i=1:length(used_y0{1})
@@ -41,7 +80,7 @@ for j=2:exps.n_exp
     y(j-1) = pe_results{j}.fit.fbest;
     x(j-1) = j;
 end
-plot(x,y);
+semilogy(x,y);
 title('fbest');
 
 
@@ -55,4 +94,15 @@ for j=2:exps.n_exp
 end
 plot(x,y);
 title('det(FIM)');
+
+
+figure;
+clear y;
+clear x;
+for j=2:exps.n_exp
+    y(j-1) = abs(det(pe_results{j}.fit.g_FIM));
+    x(j-1) = j;
+end
+semilogy(x,y)
+title('abs(det(FIM))');
 
