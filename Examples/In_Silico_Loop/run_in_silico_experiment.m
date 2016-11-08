c0d3d1feacf37be5b875526c97685e2550db2df7
Ally Hume <a.hume@ed.ac.uk>
2016-11-08 17:16:34 +0000

    Now catch and log errors when running the multiple experiments.

--- Examples/In_Silico_Loop/run_in_silico_experiment.m
+++ Examples/In_Silico_Loop/run_in_silico_experiment.m
@@ -7,8 +7,26 @@ AMIGO_Startup();
 
 cd ('Examples/In_Silico_Loop');
     
+
 for exps=1:numExperiments
-    epccOutputResultFileNameBase = [resultBase,'-',num2str(exps)];
-    gal1_run_in_silico_stepInput_noiseOnInitialValues_1Obs;
+    try
+        epccOutputResultFileNameBase = [resultBase,'-',num2str(exps)];
+        gal1_run_in_silico_stepInput_noiseOnInitialValues_1Obs;
+    catch err
+        %open file
+        errorFile = [resultBase,'-',num2str(exps),'.errorLog']
+        fid = fopen(errorFile,'a+');
+        % write the error to file
+        % first line: message
+        fprintf(fid,'%s\n',err.message);
+
+        % following lines: stack
+        for e=1:length(err.stack)
+            fprintf(fid,'%sin %s at %i\n',txt,err.stack(e).name,err.stack(e).line);
+        end
+
+        % close file
+        fclose(fid);
+    end
 end
 
