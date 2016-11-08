70a67081c772e742df69efba08e432cee3950ddc
Ally Hume <a.hume@ed.ac.uk>
2016-11-08 17:47:56 +0000

    We getting errors with variable exps when catching errors. Changed to name less likely to be used elsewhere.

--- Examples/In_Silico_Loop/run_in_silico_experiment.m
+++ Examples/In_Silico_Loop/run_in_silico_experiment.m
@@ -8,13 +8,13 @@ AMIGO_Startup();
 cd ('Examples/In_Silico_Loop');
     
 
-for exps=1:numExperiments
+for epcc_exps=1:numExperiments
     try
-        epccOutputResultFileNameBase = [resultBase,'-',num2str(exps)];
+        epccOutputResultFileNameBase = [resultBase,'-',num2str(epcc_exps)];
         gal1_run_in_silico_stepInput_noiseOnInitialValues_1Obs;
     catch err
         %open file
-        errorFile = [resultBase,'-',num2str(exps),'.errorLog'];
+        errorFile = [resultBase,'-',num2str(epcc_exps),'.errorLog'];
         fid = fopen(errorFile,'a+');
         fprintf(fid, '%s', err.getReport('extended', 'hyperlinks','off'));
         % close file
