a11c85fd9b83d0d83f1219687e157c73decc97df
Ally Hume <a.hume@ed.ac.uk>
2017-02-03 16:48:16 +0000

    Update gal1_in_silico_loop.m

--- Examples/In_Silico_Loop/gal1_in_silico_loop.m
+++ Examples/In_Silico_Loop/gal1_in_silico_loop.m
@@ -86,7 +86,7 @@ for i=1:10
     inputs.exps.u_interp{1}='step';
     inputs.exps.n_steps{1}=20; 
     inputs.exps.u{1}=repmat([1 0],1,10).*2;
-    inputs.exps.t_con{1}=union(0:36:360,10:36:360)                      % input value change points
+    inputs.exps.t_con{1}=0:18:360                    % input value change points
    
     % TODO - in a bit of a mess with the noise types that are inconsistent
     % over this loop
