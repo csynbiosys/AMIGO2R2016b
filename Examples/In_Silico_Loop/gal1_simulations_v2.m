23db2d0907d91a88bf8f45fc6dd57aa8dae5f917
Ally Hume <a.hume@ed.ac.uk>
2017-04-05 14:09:04 +0100

    Added model V2 that includes the equation for Vm1.

---
+++ Examples/In_Silico_Loop/gal1_simulations_v2.m
@@ -0,0 +1,48 @@
+
+clear model;
+results_folder = strcat('Gal1-noDelay',datestr(now,'yyyy-mm-dd-HHMMSS'));
+short_name     = 'gal1noD'
+gal1_load_model_v2;
+
+% Compile the model
+
+clear inputs;
+inputs.model = model;
+inputs.pathd.results_folder = results_folder;                        
+inputs.pathd.short_name     = short_name;
+inputs.pathd.runident       = 'initial_setup';
+AMIGO_Prep(inputs);
+
+duration = 6000;
+
+% Run a basic simulation - here we start in steady state and add gal1
+    
+inputs.exps.n_exp = 1;  % Just mocking one experiment
+    
+inputs.exps.n_obs{1}=1;                                        % Number of observed quantities per experiment                         
+inputs.exps.obs_names{1}=char('Fluorescence');                 % Name of the observed quantities per experiment    
+inputs.exps.obs{1}=char('Fluorescence=gal1_fluo');             % Observation function
+ 
+y0_to_use = [ 2.0831    1.0415    1.0415];          % Initial conditions with 'correct' parameters
+inputs.exps.exp_y0{1}=y0_to_use;           
+    
+inputs.exps.t_f{1}=duration;         % Experiment duration
+inputs.exps.n_s{1}=duration/5 + 1;   % Number of sampling times
+inputs.exps.t_s{1}=0:5:duration      % times of samples
+    
+inputs.exps.u_interp{1}='step';
+inputs.exps.n_steps{1}=6; 
+inputs.exps.u{1}=    [ 0    2    10    20  30 40];
+inputs.exps.t_con{1}=[0 1000 2000 3000 4000 5000 duration];
+   
+%inputs.plotd.plotlevel='noplot';
+    
+inputs.pathd.results_folder = results_folder;                        
+inputs.pathd.short_name     = short_name;
+inputs.pathd.runident       = 'sim';
+    
+sim = AMIGO_SModel(inputs);
+    
+
+
+
