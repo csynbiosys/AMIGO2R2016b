967d4d9192210f7f8f5b80d9ffb56b501afa5020
Filippo Menolascina <filippo.menolascina@ed.ac.uk>
2017-06-13 11:05:50 +0100

    Scrap files

---
+++ Results/gal1_LRank_1oom_oldModel/SData_gal1LRank_pp/report_gal1LRank_pp.m
@@ -0,0 +1,18 @@
+   ***********************************
+   *    AMIGO2, Copyright @CSIC      *
+   *    AMIGO2_R2016a [April 2016]    *
+   *********************************** 
+
+Date: 13-Jun-2017
+Problem folder:	 Results\gal1_LRank_1oom_oldModel
+Results folder in problem folder:	 Results\gal1_LRank_1oom_oldModel\SData_gal1LRank_pp 
+
+
+-----------------------------------------------
+ Initial value problem related active settings
+-----------------------------------------------
+ivpsolver: cvodes
+RelTol: 1e-07
+AbsTol: 1e-07
+MaxStepSize: Inf
+MaxNumberOfSteps: 1e+06
