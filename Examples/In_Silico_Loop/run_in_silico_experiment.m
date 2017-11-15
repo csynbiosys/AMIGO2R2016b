800913ce25f37d45db6f035d6c51a2c5a347ab99
ED\lbandier <lucia.bandiera@ed.ac.uk>
2017-11-15 17:54:54 +0000

    Added script for the comparison of real values vs binary random inputs.

--- Examples/In_Silico_Loop/run_in_silico_experiment.m
+++ Examples/In_Silico_Loop/run_in_silico_experiment.m
@@ -14,8 +14,10 @@ for epcc_exps=1:numExperiments
         %gal1_in_silico_loop;
         %gal1_in_silico_loop_binary_single;
         %gal1_in_silico_loop_binary_single_telegraph;
+        %gal1_in_silico_loop_single_telegraph;
         %gal1_in_silico_loop_binary_var;
-        gal1_in_silico_loop_binary_var_telegraph;
+        %gal1_in_silico_loop_binary_var_telegraph;
+        gal1_in_silico_loop_var_telegraph;
     catch err
         %open file
         errorFile = [resultBase,'-',num2str(epcc_exps),'.errorLog'];
