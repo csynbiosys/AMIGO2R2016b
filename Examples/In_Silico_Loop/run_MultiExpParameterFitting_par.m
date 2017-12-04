7c78d016c55fb6f61a42e058b628b12764790bdc
ED\lbandier <lucia.bandiera@ed.ac.uk>
2017-12-04 10:11:52 +0000

    Restrict the fitting to those parameters that are identifiable

--- Examples/In_Silico_Loop/run_MultiExpParameterFitting_par.m
+++ Examples/In_Silico_Loop/run_MultiExpParameterFitting_par.m
@@ -6,9 +6,10 @@ cd('Examples/In_Silico_Loop');
 
 parfor epcc_exps=1:numIterations
     try
-        epccOutputResultFileNameBase = [resultBase,'-',num2str(epcc_exps)];
+        epccOutputResultFileNameBase = [resultBase];
         %[out] = fit_to_acs_data_modified_f(epccOutputResultFileNameBase,epcc_exps);
-        [out] = fit_to_acs_data_modified_f_llk(epccOutputResultFileNameBase,epcc_exps);
+        %[out] = fit_to_acs_data_modified_f_llk(epccOutputResultFileNameBase,epcc_exps);
+        [out] = fit_to_acs_data_modified_f_llk_reducedPar(epccOutputResultFileNameBase,epcc_exps);
     catch err
         %open file
         errorFile = [resultBase,'-',num2str(epcc_exps),'.errorLog'];
