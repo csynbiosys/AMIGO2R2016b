b16bc04079b43d5a3040355295757f4ce2a7431f
ZGeorgeH <george-92@live.cn>
2018-01-25 16:33:44 +0000

    import the raw data from Robert

--- GeorgesPractice/GalLoop/PseudoData/GenerateData_OpenLoop.m
+++ GeorgesPractice/GalLoop/PseudoData/GenerateData_OpenLoop.m
@@ -2,8 +2,10 @@ results_folder = strcat('GalOpenLoop1',datestr(now,'yyyy-mm-dd-HHMMSS'));
 short_name     = 'galOL1';
 
 %Compile the model
+load('old.mat');
 
-testIPTG=[1e-2,1e-1,0.5,1,2,5,10,20,50,100,500,1000,5000,1e4];
+%testIPTG=[1e-2,1e-1,0.5,1,2,5,10,20,50,100,500,1000,5000,1e4];
+testIPTG=IPTG;
 
 
 clear inputs;
@@ -63,12 +65,16 @@ for i=1:inputs.exps.n_exp
     steadyAU(i)=steadyAUt(1,2)/inputs.model.par(14);
 end
 
+plot(Cit_AU,steadyAU);
+
 load('Fig2c.mat');
 figure();
-semilogx(CitrineW(:,1),CitrineW(:,2),'-o');
+semilogx(testIPTG,Cit_AU*regress(CitrineAU',Cit_AU'),'-o');
 hold on;
 %semilogx(CitrineWO(:,1),CitrineWO(:,2),'-o');
 semilogx(testIPTG,CitrineAU,'-o');
 semilogx(testIPTG,steadyAU,'-o');
+legend('Robert''s measurement (rescaled)','Simulation','Steady States');
 hold off;
+dif=CitrineAU-steadyAU;
 figure();
