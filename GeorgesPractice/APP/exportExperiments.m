4cb36a455bb9bb0ad3c5d23364f0522082156933
ZGeorgeH <george-92@live.cn>
2018-02-13 15:06:46 +0000

    improve the performance

--- GeorgesPractice/APP/exportExperiments.m
+++ GeorgesPractice/APP/exportExperiments.m
@@ -79,13 +79,13 @@ end
 
 function n = localGcd( n )
 c=length(n);
-
 while (c>1)
     if (c>2*floor(c/2))
         n=gcd(n(1:(c+1)/2),n((c+1)/2:end));
+        c=(c+1)/2;
     else
         n=gcd(n(1:c/2),n((c/2+1):end));
+        c=c/2;
     end
-    c=length(n);
 end
 end
