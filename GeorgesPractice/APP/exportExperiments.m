9dbcafbf03ac91a5b2f17883f4c34f7f20efe7e1
ZGeorgeH <george-92@live.cn>
2018-02-13 15:01:20 +0000

    finalise the experiment-actuation interface

--- GeorgesPractice/APP/exportExperiments.m
+++ GeorgesPractice/APP/exportExperiments.m
@@ -1,6 +1,21 @@
 function [duration,step,values] = exportExperiments(inputs,timeScale,fileName,varargin)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%experiment design - actuation module interface.
+%export the experiment designs in "inputs" variable to the actuation model
+%by generating a series of .txt files with the commands in the folder (one
+%file for one experiment).
+%
+%the given parameters can be (inputs,timeScale,fileName,foldername)
+% timeScale  -- in sedonds,
+% fileName   -- if the name is XXX, the name of the .txt file is XXX.txt if
+%               there is only one experiment, and XXX-1.txt, XXX-2.txt,...
+%               if there are multiple experiments.
+% foldername -- if not given, the .txt files would be generated in the 
+%               current folder.
+%
+% output     -- the output are duration(array), step(array), and
+%               values(cells).
+
+
 oldCd=cd;
 if (~isempty(varargin))
     folder=varargin{1};
@@ -25,42 +40,40 @@ for iexp=1:nofExp
             step(iexp)=duration(iexp)*timeScale;
             values{iexp}=inputs.exps.u{iexp};
         case 'step'
-            timei=inputs.exps.t_con{iexp}*timeScale;
-            gcdi=localGcd(diff(timei));
-            step(iexp)=gcdi;
-            repeati=diff(timei)/gcdi;
-            valuei=[];
+            difftimei=diff(inputs.exps.t_con{iexp}*timeScale);
+            step(iexp)=localGcd(difftimei);
+            repeati=difftimei/step(iexp);
+            values{iexp}=[];
             for i=1:length(repeati)
-                valuei=cat(2,valuei,...
-                    repmat(inputs.exps.u{iexp}(i),1,repeati(i)));
+                values{iexp}=cat(2,values{iexp},repmat(inputs.exps.u{iexp}(i),1,repeati(i)));
             end
-            values{iexp}=valuei;
         case 'pulse-up'
             timei=inputs.exps.t_con{iexp}*timeScale;
-            gcdi=localGcd(diff(timei));
-            step(iexp)=gcdi;
-            repeati=diff(timei(1:3))/gcdi;
-            valuei=cat(2,repmat(inputs.exps.u_min{iexp},1,repeati(1)),...
+            step(iexp)=localGcd(diff(timei));
+            repeati=diff(timei(1:3))/step(iexp);
+            values{iexp}=cat(2,repmat(inputs.exps.u_min{iexp},1,repeati(1)),...
                 repmat(inputs.exps.u_max{iexp},1,repeati(2)));
-            values{iexp}=repmat(valuei,1,inputs.exps.n_pulses{iexp});
+            values{iexp}=repmat(values{iexp},1,inputs.exps.n_pulses{iexp});
         case 'pulse-down'
             timei=inputs.exps.t_con{iexp}*timeScale;
-            gcdi=localGcd(diff(timei));
-            step(iexp)=gcdi;
-            repeati=diff(timei(1:3))/gcdi;
-            valuei=cat(2,repmat(inputs.exps.u_max{iexp},1,repeati(1)),...
+            step(iexp)=localGcd(diff(timei));
+            repeati=diff(timei(1:3))/step(iexp);
+            values{iexp}=cat(2,repmat(inputs.exps.u_max{iexp},1,repeati(1)),...
                 repmat(inputs.exps.u_min{iexp},1,repeati(2)));
-            values{iexp}=repmat(valuei,1,inputs.exps.n_pulses{iexp});
+            values{iexp}=repmat(values{iexp},1,inputs.exps.n_pulses{iexp});
         otherwise %linear case by default
             timei1=inputs.exps.t_con{iexp}*timeScale;
-            gcdi=localGcd(cat(2,timei1,inputs.exps.t_s{iexp}*timeScale));
-            step(iexp)=gcdi;
-            timei=0:gcdi:duration(iexp);
-            values{iexp}=interp1(timei1,inputs.exps.u{iexp},timei);
+            step(iexp)=min(d/100,localGcd(cat(2,timei1,inputs.exps.t_s{iexp}*timeScale))));
+            values{iexp}=interp1(timei1,inputs.exps.u{iexp},0:step(iexp):d);
     end
-    
+    command=[step(iexp),values{iexp}];
+    if (nofExp==1)
+        save([fileName,'.txt'],'command','-ascii');
+    else
+        save([fileName,'-',num2str(iexp),'.txt'],'command','-ascii');
+    end
 end
-save([fileName,'.mat'],'duration','step','values');
+%save([fileName,'.mat'],'duration','step','values');
 cd(oldCd);
 end
 
